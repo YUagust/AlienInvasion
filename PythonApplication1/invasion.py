79c9b4e45e6a3ce226f562f70c1778a008686f48
Maggie 女 <maggietan307@gmail.com>
2021-08-29 11:52:31 -0700

    first push

---
+++ PythonApplication1/invasion.py
@@ -0,0 +1,44 @@
+import sys
+import pygame
+import GameFunctions as gf
+from Settings import Settings
+from Ship import Ship
+from pygame.sprite import Group
+from Alien import Alien
+from game_stats import GameStats
+from button import Button
+
+def run_game():
+
+	pygame.init()
+	ai_settings = Settings()
+	screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+	pygame.display.set_caption("Alien Invation")
+
+	# Make the Play button.
+	play_button = Button(ai_settings, screen, "Play")
+
+	#Create an instance to store game statistics.
+	stats = GameStats(ai_settings)
+	# Make a ship
+	ship = Ship(ai_settings, screen)
+	# Make a group to store bullets in.
+	bullets = Group()
+	aliens = Group()
+
+	# Create the fleet of aliens.
+	gf.create_fleet(ai_settings, screen, ship, aliens)
+
+	while True:
+
+		gf.check_events(ai_settings, screen, ship, bullets)
+		if stats.game_active:		
+			ship.update()
+			gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+			gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+	
+		gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+		pygame.display.flip()
+		gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button)
+
+run_game()
